fc1ed45a8ad696b9e9d1d127360d7d13bd35a756
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2019-03-05 13:55:42 +0200

    Update Fraction.h

--- Assignment 1/Fraction Problem/Fraction.h
+++ Assignment 1/Fraction Problem/Fraction.h
@@ -1,13 +1,18 @@
 #pragma once
+#include <iostream>
+
 class Fraction
 {
 private:
 	int Num;
 	int Denom;
 	void Reduce();
+	int GCD(int a, int b);
 public:
 	Fraction();
+	Fraction(int num);
 	Fraction(int Num, int Denom);
+	Fraction(const Fraction& obj);
 
 	Fraction operator-(Fraction& Other);
 	Fraction operator+ (Fraction& Other);
@@ -23,8 +28,9 @@ public:
 	bool operator==(Fraction& Other);
 	bool operator<=(Fraction& Other);
 	bool operator>=(Fraction& Other);
-	std::istream& operator>>(std::istream& in);
-	std::ostream& operator<<(std::ostream& out);
+
+	friend std::istream& operator>>(std::istream& in , Fraction& Obj);
+	friend std::ostream& operator<<(std::ostream& out, Fraction& Obj);
 	virtual ~Fraction();
 };
 
