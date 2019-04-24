0bbe150edfeebb0f8d5920c21a4f806475b92aba
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2019-04-25 00:59:50 +0200

    Update BSTree.cpp

--- Assignment 4/BSTree.cpp
+++ Assignment 4/BSTree.cpp
@@ -22,9 +22,18 @@ node * BSTree::addInTree(node * nde, int key)
 	return nde;
 }
 
+node * BSTree::search(node * cur, int value)
+{
+	if (cur == nullptr)return nullptr;
+	else if (value < cur->value)return search(cur->left, value);
+	else if (value > cur->value) return search(cur->right, value);
+	return cur;
+}
+
 BSTree::BSTree()
 {
 	root = nullptr;
+	size = 0;
 }
 
 BSTree::BSTree(node * n)
@@ -33,6 +42,7 @@ BSTree::BSTree(node * n)
 	root->value = n->value;
 	root->left = nullptr;
 	root->right = nullptr;
+	size = 1;
 }
 
 node * BSTree::getRoot()
@@ -42,12 +52,29 @@ node * BSTree::getRoot()
 
 node * BSTree::add(int value)
 {
+	size++;
 	if (root == nullptr) {
 		root = this->newNode(value);
 		return root;
 	}
 	node * current = root;
 	return addInTree(current, value);
+}
+
+void BSTree::flip()
+{
+	this->flip(this->root);
+}
+
+void BSTree::flip(node * nde)
+{
+	if (nde == nullptr) return;
+
+	node * temp = nde->left;
+	nde->left = nde->right;
+	nde->right = temp;
+	flip(nde->left);
+	flip(nde->right);
 }
 
 void BSTree::inorder(node *root)
@@ -60,6 +87,54 @@ void BSTree::inorder(node *root)
 	}
 }
 
+void BSTree::printRange(node * root, int l, int r)
+{
+	if (root == nullptr) return;
+	
+	if (root->value < l) {
+		printRange(root->right, l, r);
+	}
+	else if (root->value >= l && root->value <= r)
+	{
+		printRange(root->left, l, r);
+		cout << root->value << endl;
+		printRange(root->right, l, r);
+	}
+	else if (root->value > r) {
+		printRange(root->left, l, r);
+	}
+	
+}
+
 BSTree::~BSTree()
 {
+}
+
+bool Compare(node * node1, node * node2)
+{
+	if (node1 == nullptr && node2 == nullptr)return 1;
+	else if (node1 == nullptr && node2 != nullptr) return 0;
+	else if (node1 != nullptr && node2 == nullptr)return 0;
+	if (node1->value == node2->value)
+		return 1 * Compare(node1->left, node2->left) * Compare(node1->right, node2->right);
+	else
+		return 0;
+}
+
+bool isSubTree(BSTree * Tree1, BSTree * Tree2)
+{
+	if (Tree1 == nullptr || Tree2 == nullptr)return 0;
+	if (Tree2->size > Tree1->size) {
+		BSTree * temp = Tree2;
+		Tree2 = Tree1;
+		Tree1 = temp;
+	}
+	node * temp = Tree1->root;
+	node * root2 = Tree2->root;
+
+	node * nde = Tree1->search(temp, root2->value);
+	if (nde == nullptr)return 0;
+
+	if (Compare(nde, root2))return 1;
+	return 0;
 }
